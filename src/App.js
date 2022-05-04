c77ad51c5c9a9c9e0b3193d33b19d214c775f61d
user.email <elaine.t.huynh@gmail.com>
2022-05-04 00:38:39 -0700

    Created components folder and pages file, added data and styled using node modules

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pages from './pages/Pages'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title">My Recipe App</h1>
+      <Pages />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Veggie.jsx
@@ -0,0 +1,106 @@
+import React, {useEffect, useState} from 'react';
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import '@splidejs/splide/dist/css/splide.min.css';
+import styled from "styled-components";
+
+function Veggie() {
+
+    const [vegetarian, setVegetarian] = useState([]);
+
+
+    useEffect(() => {
+        getVegetarian();
+    }, []);
+
+    const getVegetarian = async () => {
+
+        const check = localStorage.getItem('vegetarian');
+
+        if(check) {
+            setVegetarian(JSON.parse(check))
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
+            );
+            const data = await api.json();
+
+            localStorage.setItem("vegetarian", JSON.stringify(data.recipes));
+            setVegetarian(data.recipes);
+            console.log(data.recipes);
+        }
+    };
+
+    return (
+    <div>
+        <Wrapper>
+            <h3>Vegetarian Recipes</h3>
+
+            <Splide options={{
+                perPage: 4,
+                arrows: true,
+                pagination: false,
+                drag: "free",
+                gap: "5rem"
+            }}>
+                {vegetarian.map((recipe) => {
+                    return (
+                        <SplideSlide key={recipe.id}>
+                            <Card>
+                                <p>{recipe.title}</p>
+                                <img src={recipe.image} alt={recipe.title}/>
+                                <Gradient />
+                            </Card>
+                        </SplideSlide>
+                    );
+                })}
+            </Splide>
+        </Wrapper>
+    </div>
+    )
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem
+`;
+const Card = styled.div`
+  min-height: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img{
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+  }
+  
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`;
+
+export default Veggie;
+
